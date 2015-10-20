fdc8fd866334f07ce0a793285505713ae2161f59
Erik Rogers <erik.rogers@live.com>
2015-10-19 21:33:31 -0400

    Added Update classes

---
+++ SleepHunter/Updates/BuildHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SleepHunter.Updates
+{
+   public static class BuildHelper
+   {
+      static readonly Regex BuildTimeRegex = new Regex(@"\s*(?<year>[0-9]{2})(?<month>[0-9A-Fa-f]{1})(?<day>[0-9]{2})(?<revision>[0-9A-Fa-f]?)\s*");
+
+      public static bool TryGetBuildTime(this string build, out DateTime date, out int revision)
+      {
+         date = DateTime.MinValue;
+         revision = 0;
+         var match = BuildTimeRegex.Match(build);
+
+         if (!match.Success)
+            return false;
+
+         int year;
+         int month;
+         int day;
+
+         if (!int.TryParse(match.Groups["year"].Value, out year))
+            return false;
+
+         if (!int.TryParse(match.Groups["month"].Value, NumberStyles.HexNumber, null, out month))
+            return false;
+
+         if (!int.TryParse(match.Groups["day"].Value, out day))
+            return false;
+
+         if (!int.TryParse(match.Groups["revision"].Value, NumberStyles.HexNumber, null, out revision))
+            return false;
+
+         date = new DateTime(year + 2000, month, day);
+         return true;
+      }
+
+      public static bool TryCompareBuild(string buildA, string buildB, out int result)
+      {
+         result = -2;
+
+         DateTime buildTimestampA, buildTimestampB;
+         int buildRevisionA, buildRevisionB;
+
+         if (!TryGetBuildTime(buildA, out buildTimestampA, out buildRevisionA))
+            return false;
+
+         if (!TryGetBuildTime(buildB, out buildTimestampB, out buildRevisionB))
+            return false;
+
+         if (buildTimestampA > buildTimestampB)
+            result = 1;
+         else if (buildTimestampB > buildTimestampA)
+            result = -1;
+         else if (buildTimestampA == buildTimestampB)
+         {
+            if (buildRevisionA > buildRevisionB)
+               result = 1;
+            else if (buildRevisionB > buildRevisionA)
+               result = -1;
+            else
+               result = 0;
+         }
+
+         return true;
+      }
+
+   }
+}
---
+++ SleepHunter/Updates/SoftwareBundle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SleepHunter.Updates
+{
+   public delegate void SoftwareBundleCallback(SoftwareBundle bundle);
+
+   [Serializable]
+   [XmlRoot("SoftwareBundle")]
+   public sealed class SoftwareBundle
+   {
+      string repositoryUrl;
+      List<SoftwareBundleFile> files;
+
+      [XmlAttribute("Repository")]
+      public string RepositoryUrl
+      {
+         get { return repositoryUrl; }
+         set { repositoryUrl = value; }
+      }
+
+      [XmlArray("Files")]
+      [XmlArrayItem("File")]
+      public List<SoftwareBundleFile> Files
+      {
+         get { return files; }
+         private set { files = value; }
+      }
+
+      public SoftwareBundle()
+      {
+         files = new List<SoftwareBundleFile>();
+      }
+
+      public SoftwareBundle(int capacity)
+      {
+         files = new List<SoftwareBundleFile>(capacity);
+      }
+
+      public SoftwareBundle(IEnumerable<SoftwareBundleFile> collection)
+      {
+         files = new List<SoftwareBundleFile>(collection);
+      }
+
+      public static SoftwareBundle LoadFromFile(string file)
+      {
+         using (var inputStream = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+         {
+            var bundle = LoadFromStream(inputStream);
+            inputStream.Close();
+
+            return bundle;
+         }
+      }
+
+      public static SoftwareBundle LoadFromStream(Stream stream)
+      {
+         var serializer = new XmlSerializer(typeof(SoftwareBundle));
+         var bundle = serializer.Deserialize(stream) as SoftwareBundle;
+
+         return bundle;
+      }
+
+      public void SaveToFile(string file)
+      {
+         using (var outputStream = File.Create(file))
+         {
+            SaveToStream(outputStream);
+            outputStream.Close();
+         }
+      }
+
+      public void SaveToStream(Stream stream)
+      {
+         var serializer = new XmlSerializer(typeof(SoftwareBundle));
+         var namespaces = new XmlSerializerNamespaces();
+         namespaces.Add("", "");
+
+         serializer.Serialize(stream, this, namespaces);
+      }
+   }
+}
