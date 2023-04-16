2581712dcf4b8b5451b79483f183e243347c24a8
FallenDev <Paul.Buck@TheBuckNetwork.com>
2023-04-16 15:35:38 -0400

    feat: SleepHunter.Updater Proj Conversion

---
+++ SleepHunter4.Updater/Obsidian.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace SleepHunter.Updater
+{
+    internal partial class Obsidian
+    {
+        protected virtual void WindowThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (!(sender is Thumb thumb))
+                return;
+
+            if (!(thumb.TemplatedParent is Window window))
+                return;
+
+            window.Left += e.HorizontalChange;
+            window.Top += e.VerticalChange;
+        }
+
+        protected virtual void WindowThumb_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (!(sender is Thumb thumb))
+                return;
+
+            if (!(thumb.TemplatedParent is Window window))
+                return;
+
+            if (window.ResizeMode == ResizeMode.NoResize)
+                return;
+
+            if (e.LeftButton.HasFlag(MouseButtonState.Pressed))
+            {
+                if (window.WindowState == WindowState.Maximized)
+                    window.WindowState = WindowState.Normal;
+                else
+                    window.WindowState = WindowState.Maximized;
+            }
+        }
+
+        protected virtual void WindowMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            if (!(button.TemplatedParent is Window window))
+                return;
+
+            if (window.WindowState == WindowState.Minimized)
+                window.WindowState = WindowState.Normal;
+            else
+                window.WindowState = WindowState.Minimized;
+        }
+
+        protected virtual void WindowMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            if (!(button.TemplatedParent is Window window))
+                return;
+
+            if (window.WindowState == WindowState.Maximized)
+                window.WindowState = WindowState.Normal;
+            else
+                window.WindowState = WindowState.Maximized;
+        }
+
+        protected virtual void WindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            if (!(button.TemplatedParent is Window window))
+                return;
+
+            window.Close();
+        }
+
+        protected virtual void TextBoxClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            if (!(button.TemplatedParent is TextBox textBox))
+                return;
+
+            textBox.Clear();
+            textBox.Focus();
+        }
+    }
+}
---
+++ SleepHunter4.Updater/Win32/NativeMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SleepHunter.Updater.Win32
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32", EntryPoint = "SetForegroundWindow", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool SetForegroundWindow(IntPtr windowHandle);
+    }
+}
