958eccebc294882c0c3906b3a6bbec1325eef32b
FallenDev <Paul.Buck@TheBuckNetwork.com>
2023-04-16 14:43:47 -0400

    feat: Adding new .NET 7 Projects, and Transitioning .NET Framework Projects - Initial Setup

---
+++ SleepHunter4/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SleepHunter4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
